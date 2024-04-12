ae097ddea2387fdf44ad117bf707931bfe1aa65a
joao <joao@platon.com.br>
2024-04-12 13:07:32 -0300

    chore: adicionado prisma e modelos de tabela.

--- backend/src/helpers/logger/logger.service.spec.ts
+++ backend/src/helpers/logger/logger.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { LoggerService } from './logger.service';
+import { CustomLogger } from './logger.service';
 
 describe('LoggerService', () => {
-  let service: LoggerService;
+  let service: CustomLogger;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LoggerService],
+      providers: [CustomLogger],
     }).compile();
 
-    service = module.get<LoggerService>(LoggerService);
+    service = module.get<CustomLogger>(CustomLogger);
   });
 
   it('should be defined', () => {
