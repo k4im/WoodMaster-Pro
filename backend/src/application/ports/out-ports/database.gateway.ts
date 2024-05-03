5a1b27cab3abd67101db6a7bda38c2f12a9e397b
joao <joao@platon.com.br>
2024-05-03 17:44:31 -0300

    chore[BREAKING_CHANGE]: refatorado organizacao de pastas.

--- backend/src/application/ports/out-ports/database.gateway.ts
+++ backend/src/application/ports/out-ports/database.gateway.ts
@@ -1,8 +1,3 @@
-import { Prisma } from "@prisma/client";
-import { DefaultArgs } from "@prisma/client/runtime/library";
-import { ITenantDto } from "src/domain/dto/ITenant.dto";
-import { IPessoaDto } from "src/domain/dto/Pessoas.dto";
-import { filtro } from "src/domain/enum/filtroPaginacao.enum";
 import { DataSource } from "typeorm";
 
 export interface DatabaseGateway {
