5f02c92e100dc08f3027809a2038b1953a7e303a
joao <joao@platon.com.br>
2024-05-10 17:38:00 -0300

    fix(tests): adicionado --runInBand no script de test.

--- backend/src/infrastructure/logger/Fakelogger.service.ts
+++ backend/src/infrastructure/logger/Fakelogger.service.ts
@@ -4,10 +4,10 @@ import { LogLevel } from 'src/application/enum/logger.enum';
 import { LoggerGateway } from 'src/application/ports/out-ports/logger.gateway';
 
 @Injectable()
-export class FakeLogger  implements LoggerService, LoggerGateway{
-    
-      log(message: any, ...optionalParams: any[]) {
-        console.log(`${LogLevel.INFO}INFO: ${LogLevel.RESET}${message}`)
+export class FakeLogger implements LoggerService, LoggerGateway {
+
+    log(message: any, ...optionalParams: any[]) {
+        // console.log(`${LogLevel.INFO}INFO: ${LogLevel.RESET}${message}`)
     }
 
     error(message: any, ...optionalParams: any[]) {
