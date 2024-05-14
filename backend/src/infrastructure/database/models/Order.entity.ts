71871a01d03695490d1621b29f19e760612a201f
joao <joao@platon.com.br>
2024-05-14 16:56:04 -0300

    feat: adicionado entidade de order e orderItem.

---
+++ backend/src/infrastructure/database/models/Order.entity.ts
@@ -0,0 +1,28 @@
+import { Address } from "src/domain/valueObjects/AddressVo/address.value.object";
+import { Column, CreateDateColumn, Entity, Generated, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import OrderItem from "./OrderItem.entity";
+import { Tenant } from "./Tenant.entity";
+
+@Entity()
+export default class Order  {
+    @PrimaryGeneratedColumn()
+    Id: number
+    @Column({type: 'uuid', nullable: true})
+    @Generated("uuid")
+    Uuid: string
+    @Column({nullable: false})
+    OrderType: string
+    @Column({nullable: true, type: 'json'})
+    DeliveryAddress: Address
+
+    @OneToMany(() => OrderItem, (ot) => ot.OrderId, {onUpdate: "CASCADE", onDelete: "SET NULL"})
+    OrderItens: OrderItem[]
+
+    @ManyToOne(() => Tenant, (t) => t.Orders, {onUpdate: "CASCADE", onDelete: "SET NULL"})
+    Tenant: Tenant
+
+    @CreateDateColumn()
+    createAt: Date;
+    @UpdateDateColumn()
+    updateAt: Date
+}
--- backend/src/infrastructure/database/models/Tenant.entity.ts
+++ backend/src/infrastructure/database/models/Tenant.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, Generated, Index, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, Generated, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Stock } from "./Stock.entity";
 import { User } from "./User.entity";
 import { Person } from "./Person.entity";
+import Order from "./Order.entity";
 
 @Entity()
 export class Tenant { 
@@ -24,4 +25,13 @@ export class Tenant {
     
     @OneToMany(() => Person, (user) => user.Tenant, {onUpdate: "CASCADE", onDelete: "SET NULL"})
     Persons: Person[]
+    
+    @OneToMany(() => Order, (or) => or.Tenant, {onUpdate: "CASCADE", onDelete: "SET NULL"})
+    Orders: Order[]
+
+
+    @CreateDateColumn()
+    createAt: Date;
+    @UpdateDateColumn()
+    updateAt: Date
 }
