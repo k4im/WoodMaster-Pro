074626e1c88424f37f8075931951aaea34c938a0
João Victor <joao@platon.com.br>
2024-06-28 09:54:54 -0300

    refactor(usecase): movido pastas de usecases.

--- backend/src/infrastructure/utils/env.checker.ts
+++ backend/src/infrastructure/utils/env.checker.ts
@@ -1,20 +1,20 @@
 class EnvChecker  {
 
     checkEnv() { 
-        if(!process.env.HOST) 
-            throw Error("É necessário espeficicar a URL do banco de dados.")
-        if(!process.env.PORT_DB)
-          throw Error("É necessário espeficicar a porta de acesso ao banco de dados.")
-        if(!process.env.USER_DB)
-          throw Error("É necessário espeficicar o usuario do banco de dados.")
-        if(!process.env.PWD_DB)
-          throw Error("É necessário espeficicar a senha de acesso ao banco de dados.")
-        if(!process.env.DB_NAME)
-          throw Error('É necessário especificar o nome do banco')
-        if(!process.env.PORT_APP) 
-          throw Error("A porta do app precisa ser definida")
-        if(!process.env.SECRET_KEY)
-          throw Error('Secret key not informed.')      
+      if(!process.env.HOST) 
+          throw Error("É necessário espeficicar o HOST do banco de dados.")
+      if(!process.env.PORT_DB)
+        throw Error("É necessário espeficicar a porta de acesso ao banco de dados.")
+      if(!process.env.USER_DB)
+        throw Error("É necessário espeficicar o usuario do banco de dados.")
+      if(!process.env.PWD_DB)
+        throw Error("É necessário espeficicar a senha de acesso ao banco de dados.")
+      if(!process.env.DB_NAME)
+        throw Error('É necessário especificar o nome do banco')
+      if(!process.env.PORT_APP) 
+        throw Error("A porta do app precisa ser definida")
+      if(!process.env.SECRET_KEY)
+        throw Error('Secret key not informed.')      
     }
 }
 
